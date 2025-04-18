1f2450bccb73bf438bc7b3513799cc4678ad9cb1
liuhong <1626453902@qq.com>
2025-04-18 16:40:30 +0800

    feat: 🎨 update

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import getAxisMaxAndMin from './getAxisMaxAndMin'
 import Ajax from './ajax'
 import { PENDING } from './ajax/abstract'
 import Loop from './loop'
+import LhResizeObserver from './LhResizeObserver'
 
-export { arithmetic, getAxisMaxAndMin, Ajax, PENDING, Loop }
+export { arithmetic, getAxisMaxAndMin, Ajax, PENDING, Loop, LhResizeObserver }
