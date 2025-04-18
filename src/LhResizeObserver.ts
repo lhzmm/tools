2052aed7afdd3c392d780c807cdc879256eb6d29
liuhong <1626453902@qq.com>
2025-04-18 16:03:46 +0800

    feat: 🎨 LhResizeObserver

---
+++ src/LhResizeObserver.ts
@@ -0,0 +1,85 @@
+
+import 'resize-observer-polyfill'
+
+class LhResizeObserver {
+  private targetMapToCallback: Map<Element, Function[]> = new Map()
+  private ob: ResizeObserver
+  // 忽略1次ResizeObserver的回调
+  private ignoreResizeObserverOnce = false
+
+  constructor() {
+    this.ob = new ResizeObserver(this.schedule.bind(this))
+  }
+
+  /**
+   * 调度
+   * @param entries ResizeObserverEntry[]
+   * @param observer ResizeObserver
+   */
+  private schedule(entries: ResizeObserverEntry[], observer: ResizeObserver): void {
+    if (this.ignoreResizeObserverOnce) {
+      this.ignoreResizeObserverOnce = false
+      return
+    }
+    entries.forEach((entry) => {
+      const callbacks = this.targetMapToCallback.get(entry.target)
+      if (callbacks && callbacks.length) {
+        callbacks.forEach((cb) => cb(entry))
+      }
+    })
+  }
+
+  /**
+   * 解除元素大小监听
+   * @param target 被监听的目标元素
+   */
+  unobserve(target: Element, callback?: Function): void {
+    if (this.targetMapToCallback.has(target)) {
+      if (callback) {
+        const cbs = this.targetMapToCallback.get(target)!
+        const fidx = cbs.indexOf(callback)
+        // 存在对应的要移除的callback
+        if (fidx > -1) {
+          // 移除后清空
+          if (cbs.length === 1) {
+            this.targetMapToCallback.delete(target)
+            this.ob.unobserve(target)
+          } else {
+            // 移除对应的callback
+            cbs.splice(fidx, 1)
+          }
+        }
+      } else {
+        // 未提供第二个参数时，清空该元素的所有
+        this.targetMapToCallback.delete(target)
+        this.ob.unobserve(target)
+      }
+    }
+  }
+
+  /**
+   * 对目标元素添加监听大小变化的回调
+   * @param target 目标元素
+   * @param callback 监听回调
+   */
+  observe(target: HTMLElement, callback: Function): void {
+    if (this.targetMapToCallback.has(target)) {
+      this.targetMapToCallback.set(target, this.targetMapToCallback.get(target)!.concat([callback]))
+    } else {
+      this.targetMapToCallback.set(target, [callback])
+    }
+    const visible = !!(target.offsetWidth || target.offsetHeight || target.getClientRects().length)
+    if (visible) {
+      /**
+       * 对可见元素初次observe时，会触发schedule
+       * 但设计的是当元素的size变化时，触发schedule
+       * 故忽略这次的schedule
+       */
+      this.ignoreResizeObserverOnce = true
+    }
+    this.ob.observe(target)
+  }
+}
+
+export default LhResizeObserver
+
